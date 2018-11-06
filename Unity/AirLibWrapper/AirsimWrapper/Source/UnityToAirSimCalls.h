0d87fa0a1059879db5d6913ca909d2ab879a9699
msb336 <msb336@drexel.edu>
2018-11-06 14:08:03 -0800

    library full compiles. Current issue I think may be specific to my compiler settings. Hopefully it isn't related to incompatibility between g++-5 and g++-4.8

--- Unity/AirLibWrapper/AirsimWrapper/Source/UnityToAirSimCalls.h
+++ Unity/AirLibWrapper/AirsimWrapper/Source/UnityToAirSimCalls.h
@@ -5,11 +5,17 @@
 #include "SimHUD/SimHUD.h"
 #include "Logger.h"
 
+#ifdef _WIN32
+	#define EXPORT __declspec(dllexport)
+#else
+	#define EXPORT __attribute__((visibility("default")))
+#endif
+
 static SimHUD* key = nullptr;
 
 void StartServerThread(std::string vehicle_name, std::string sim_mode_name, int port_number);
 
-extern "C" __declspec(dllexport) bool StartServer(char* vehicle_name, char* sim_mode_name, int port_number)
+extern "C" EXPORT bool StartServer(char* vehicle_name, char* sim_mode_name, int port_number)
 {
 	LOGGER->WriteLog("Starting server for : " + std::string(sim_mode_name));
 	std::thread server_thread(StartServerThread, vehicle_name, sim_mode_name, port_number);
@@ -23,7 +29,7 @@ extern "C" __declspec(dllexport) bool StartServer(char* vehicle_name, char* sim_
 	return key->server_started_Successfully_;
 }
 
-extern "C" __declspec(dllexport) void StopServer(char* vehicle_name)
+extern "C" EXPORT void StopServer(char* vehicle_name)
 {
 	key->EndPlay();
 	if (key != nullptr)
@@ -35,12 +41,12 @@ extern "C" __declspec(dllexport) void StopServer(char* vehicle_name)
 	LOGGER->WriteLog("Server stopped");
 }
 
-extern "C" __declspec(dllexport) void CallTick(float deltaSeconds)
+extern "C" EXPORT void CallTick(float deltaSeconds)
 {
 	key->Tick(deltaSeconds);
 }
 
-extern "C" __declspec(dllexport) void InvokeCollisionDetection(AirSimUnity::AirSimCollisionInfo collision_info)
+extern "C" EXPORT void InvokeCollisionDetection(AirSimUnity::AirSimCollisionInfo collision_info)
 {
 	auto simMode = key->GetSimMode();
 	if (simMode)
